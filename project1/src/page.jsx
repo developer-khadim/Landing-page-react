9796508b9c35e81b539590677343b1f5d8970892
Your Name <you@example.com>
2024-06-27 02:14:24 +0500

    full

--- project1/src/page.jsx
+++ project1/src/page.jsx
@@ -1,11 +1,17 @@
 "use client";
 import React from 'react'
 import Nav from '../components/nav';
+import Hero from '../components/hero';
 
 const Page = () => {
   return (
-    <main>
-    <Nav/>
+    <main className=" max-w-[100vw] h-[90vh] " >
+    <div className="nav ">
+    <Nav />
+    </div>
+    <div className="hero h-[85vh] flex justify-center  ">
+      < Hero className="w-[80vw " />
+    </div>
     </main>
   )
 }
