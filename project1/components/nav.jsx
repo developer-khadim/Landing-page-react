e95fa97c1133e0506663cf8eaf733350877fb3a0
Your Name <you@example.com>
2024-06-26 23:38:42 +0500

    nav

---
+++ project1/components/nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const Nav = () => {
+  return (
+    <div>
+      <nav className='text-red-400' >
+        <div><img src="./src/assetbrand_logo.png" alt="b" /></div>
+        <div>
+           <h3>M</h3>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav
---
+++ project1/src/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react'
+import Nav from '../components/nav';
+
+const Page = () => {
+  return (
+    <main>
+    <Nav/>
+    </main>
+  )
+}
+
+export default Page
